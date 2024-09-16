3aed7a48e1040a5735d044df1d8da1739f31dc8a
Sahilchouhan20 <sahilchouhan0103pl@gmail.com>
2024-09-16 19:31:17 +0530

    Add a JWT in api

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    devise_for :users, path: '', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+  end
 end
